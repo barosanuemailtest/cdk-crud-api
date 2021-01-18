a0ac32b593f75bd249d1596bbbe650b2397e94c5
barosanuemailtest <barosanuemailtest@gmail.com>
2021-01-18 11:51:43 +0200

    webpack working configuration

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,19 +2,25 @@ import * as path from 'path';
 import * as webpack from 'webpack';
 
 const config: webpack.Configuration = {
-    mode: 'production',
+    mode: 'none',
     entry: './services/lib/hello/HelloHandler.ts',
     module: {
         rules: [
             {
-                use: 'ts-loader',
-                exclude: /node_modules/
+                use: {
+                    loader: 'ts-loader',
+                    options: {
+                        configFile: "tsconfig.webpack.json"
+                    }
+                },
+                exclude: /node_modules/,
             }
         ]
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js']
     },
+    // devtool: "source-map",
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'foo.bundle.js'
