2053e1ffbc60f0b34069c41c1f79e11c0a5e7afd
barosanuemailtest <barosanuemailtest@gmail.com>
2021-01-18 18:14:01 +0200

    Lambda tests

--- cloud_infrastructure/bin/stacks/Lambdas.ts
+++ cloud_infrastructure/bin/stacks/Lambdas.ts
@@ -1,14 +1,18 @@
-import { Code, Function as LambdaFunction, Runtime} from '@aws-cdk/aws-lambda';
+import { Code, Function as LambdaFunction, Runtime } from '@aws-cdk/aws-lambda';
 import { Construct } from '@aws-cdk/core';
+import { join } from 'path';
 
 
 
-export function getHelloLambda(scope: Construct):LambdaFunction{
-return new LambdaFunction(scope, 'HelloHandler', {
-    functionName: 'HelloHandler',
-    code: Code.fromInline(`console.log('Hello from lambda!')`),
-    handler: 'zzz',
-    runtime: Runtime.NODEJS_12_X
-})
+export function getHelloLambda(scope: Construct): LambdaFunction {
 
+    const functionCode = Code.fromAsset(join(__dirname, '..', '..', '..','build'));
+
+    const someLambda = new LambdaFunction(scope, 'HelloHandler', {
+        functionName: 'HelloHandler',
+        code: functionCode,
+        handler: 'handler',
+        runtime: Runtime.NODEJS_12_X
+    });
+    return someLambda
 }
---
+++ cloud_infrastructure/test/LambdaPathTest.test.ts
@@ -0,0 +1,15 @@
+
+import { Code, Function} from '@aws-cdk/aws-lambda';
+import { Stack } from '@aws-cdk/core';
+import { join } from 'path';
+import { getHelloLambda } from '../bin/stacks/Lambdas';
+
+describe('sample test suite', ()=>{
+
+    test('Lambda Code from path',()=>{
+        const someStack = new Stack();
+        const LambdaFunction: Function = getHelloLambda(someStack);
+        console.log(5)
+    })
+
+});
